18ac27f9f7a82f37f10ee6d2768996ea72af3b0f
Marius <marius.budreika09@gmail.com>
2022-05-08 12:29:20 +0300

    fixed date

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -2,13 +2,18 @@ import React from "react";
 import styles from "./WeatherInfo.module.scss";
 
 const WeatherInfo = ({ data: { location, current } }) => {
+  const formattedDate = current.last_updated.substring(
+    0,
+    current.last_updated.length - 6
+  );
+
   return (
     <div className={styles.box}>
       <div className={styles.topContainer}>
         <div className={styles.topContainerLocation}>
           {location.country} , {location.name}
         </div>
-        <div className={styles.topContainerDate}>2022-05-06</div>
+        <div className={styles.topContainerDate}>{formattedDate}</div>
       </div>
       <div className={styles.bottomContainer}>
         <div className={styles.bottomContainerTemperature}>
